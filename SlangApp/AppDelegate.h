c8df39548b9dd1bfe9559d1072876e4108430aa3
VictorBX <vbarr004@ucr.edu>
2015-03-07 13:55:40 -0800

    Initial Commit

---
+++ SlangApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlangApp
+//
+//  Created by Victor Barrera on 3/7/15.
+//  Copyright (c) 2015 Victor Barrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SlangApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SlangApp
+//
+//  Created by Victor Barrera on 3/7/15.
+//  Copyright (c) 2015 Victor Barrera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
